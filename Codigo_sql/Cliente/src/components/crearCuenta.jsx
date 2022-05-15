e0bd361bc95247d2f7df232d686a72e9e9c71024
LukasPinto <lukas.pinto@alumnos.uv.cl>
2022-05-14 23:09:42 -0400

    listado direcciones service

--- Codigo_sql/Cliente/src/components/crearCuenta.jsx
+++ Codigo_sql/Cliente/src/components/crearCuenta.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import "./css/crearCuenta.css"
 import { Form, Button, FormLabel } from 'react-bootstrap';
 import img1 from './../assets/img/usuario.png';
-import { useState } from 'react';
-
+import { useState , useEffect} from 'react';
+import direccionesService from '../services/listadoDirecciones.service';
 const CrearCuenta = (props) => {
 
     /*const id_direccion = body.correo
@@ -12,70 +12,90 @@ const CrearCuenta = (props) => {
     const rut = body.rut
     const correo = body.correo
     const clave = body.clave*/
-    const [body, setBody] =
-        useState({
-            correo: '',
-            clave: ''
-        });
-    const handleChange = async (e) => {
-        await setBody({
-            ...body,
-            [e.target.name]: e.target.value
-        })
-        console.log(body.correo, body.clave)
-    }
+    const [direcciones, setDirecciones] = useState()
+    const [actualizar, setActualizar] = useState(true)
+    const [traerDatos, setTraerDatos] = useState(true)
+    useEffect(() => {
+        direccionesService()
+            .then((Response) => {
+                console.log(Response)
+                setDirecciones(Response.data)
+                setActualizar(!actualizar)
+            }).
+            catch(() => {
+                alert("error")
+            })
+    }, [traerDatos])
+
+
+const [body, setBody] =
+    useState({
+        id_direccion :"",
+        id_cargo :"",
+        nombre :"",
+        rut :"",
+        correo : "",
+        clave : ""
+    });
+const handleChange = async (e) => {
+    await setBody({
+        ...body,
+        [e.target.name]: e.target.value
+    })
+    console.log(body.id_direccion ,body.id_cargo ,body.nombre ,body.rut ,body.correo , body.clave )
+}
 
 
 
-    return (
+return (
 
-        <div className="contenedor">
-            <Form >
-                <img className='imagen' src={img1} />
-                <h5 className='outer-h5'>Crear usuario</h5>
+    <div className="contenedor">
+        <Form >
+            <img className='imagen' src={img1} />
+            <h5 className='outer-h5'>Crear usuario</h5>
 
-                <div className="row justify-content-center">
-                    <Form.Group className="mb-3 col col-md-8 mt-auto ">
-                        <FormLabel className='outer-text' >Nombre Apellido</FormLabel>
-                        <Form.Control type="text" className="outer-control"
-                            name='nombre'
-                            value={body.nombre}
-                            onChange={handleChange} />
-
-
-                        <Form.Label className='outer-text'>Direccion</Form.Label>
-                        <Form.Control type="text" className="outer-control" name='correo' />
+            <div className="row justify-content-center">
+                <Form.Group className="mb-3 col col-md-8 mt-auto ">
+                    <FormLabel className='outer-text' >Nombre Apellido</FormLabel>
+                    <Form.Control type="text" className="outer-control"
+                        name='nombre'
+                        value={body.nombre}
+                        onChange={handleChange} />
 
 
-                        <Form.Label className='outer-text'>Correo</Form.Label>
-                        <Form.Control className="outer-control" type="email" />
+                    <Form.Label className='outer-text'>Direccion</Form.Label>
+                    <Form.Control type="text" className="outer-control" name='correo' />
 
 
-                        <Form.Label className='outer-text'>Contraseña</Form.Label>
-                        <Form.Control type="password" className="outer-control" />
+                    <Form.Label className='outer-text'>Correo</Form.Label>
+                    <Form.Control className="outer-control" type="email" />
+
+
+                    <Form.Label className='outer-text'>Contraseña</Form.Label>
+                    <Form.Control type="password" className="outer-control" />
 
 
 
-                        <Form.Label className='outer-text'>Cargo</Form.Label>
-                        <Form.Select type="email" className="outer-control"   >
-                            <option className="outer-text">Seleccione el cargo del usuario</option>
-                            <option value="1">Usuario de direccion</option>
-                            <option value="2">Subalterno de direccion</option>
-                            <option value="3">Administrador</option>
-                        </Form.Select>
-                    </Form.Group>
+                    <Form.Label className='outer-text'>Cargo</Form.Label>
+                    <Form.Select type="email" className="outer-control"   >
+                        <option className="outer-text">Seleccione el cargo del usuario</option>
+                        <option value="1">Usuario de direccion</option>
+                        <option value="2">Subalterno de direccion</option>
+                        <option value="3">Administrador</option>
+                    </Form.Select>
+                </Form.Group>
 
 
-                    <Button variant="outline-success" className="boton1">
-                        Crear usuario
-                    </Button>{' '}
+                <Button variant="outline-success" className="boton1">
+                    Crear usuario
+                </Button>{' '}
 
-                </div>
-            </Form>
-        </div>
+            </div>
+        </Form>
+    </div>
 
 
-    );
+);
 };
 
 
--- Codigo_sql/Cliente/src/services/listadoDirecciones.service.jsx
+++ Codigo_sql/Cliente/src/services/listadoDirecciones.service.jsx
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-export default function direccionesService(rut){
+export default function direccionesService(){
     return  (Axios.get(`http://localhost:3001/direcciones`, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
