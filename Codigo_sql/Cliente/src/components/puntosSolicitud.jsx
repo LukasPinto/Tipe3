aaf87f8bdeac4f3eb8f68dbaf5da716689db3a53
Raigou <angel.araya@alumnos.uv.cl>
2022-05-15 01:36:29 -0400

    tabla puntos solicitud (beta)

--- Codigo_sql/Cliente/src/components/puntosSolicitud.jsx
+++ Codigo_sql/Cliente/src/components/puntosSolicitud.jsx
@@ -5,7 +5,7 @@ const PuntosSolicitud = (props) => {
     return (
         <>
         <div>
-        <p class="cajita1">Historial de solicitud</p>
+        <p class="cajita1">Puntos de la solicitud</p>
         </div>
         <div>
         <Button className='botoncito2'>ORDENAR</Button>{' '}
@@ -15,45 +15,60 @@ const PuntosSolicitud = (props) => {
   <thead>
     <tr>
       
+      <th>Puntos</th>
+      <th>Descripcion del punto</th>
+      <th>Asignar encargado</th>
+      <th>Eliminar punto</th>
+      <th>Editar punto</th>
+      <th>Historial</th>
       <th>Fecha inicio</th>
       <th>Fecha termino</th>
-      <th>Expediente</th>
       <th>Estado</th>
-      <th>Detalle de solicitud</th>
       
     </tr>
   </thead>
   <tbody>
     <tr>
       
+      <td>1</td>
+      <td>Resumen de juicios</td>
+      <td><Button variant="primary">ASIGNAR</Button>{' '}</td>
+      <td><Button variant="primary">ELIMINAR</Button>{' '}</td>
+      <td><Button variant="primary">EDITAR</Button>{' '}</td>
+      <td><Button variant="primary">VER</Button>{' '}</td>
       <td>23-04-2021</td>
-      <td>25-04-2021</td>
-      <td>Cuenta publica de transito - 23-04-2021 - Direccion de Transito</td>
-      <td>INCOMPLETO</td>
-      <td><Button variant="primary">VER</Button>{' '}</td>
+      <td></td>
+      <td>RECHAZADO</td>
     </tr>
     <tr>
       
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td>2</td>
+      <td>Resumen de finanzas</td>
+      <td><Button variant="primary">ASIGNAR</Button>{' '}</td>
+      <td><Button variant="primary">ELIMINAR</Button>{' '}</td>
+      <td><Button variant="primary">EDITAR</Button>{' '}</td>
       <td><Button variant="primary">VER</Button>{' '}</td>
+      <td>23-07-2021</td>
+      <td></td>
+      <td>PENDIENTE</td>
     </tr>
     <tr>
       
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+      <td>3</td>
+      <td>Metricas</td>
+      <td><Button variant="primary">ASIGNAR</Button>{' '}</td>
+      <td><Button variant="primary">ELIMINAR</Button>{' '}</td>
+      <td><Button variant="primary">EDITAR</Button>{' '}</td>
       <td><Button variant="primary">VER</Button>{' '}</td>
+      <td>30-01-2022</td>
+      <td>04-02-2022</td>
+      <td>ACEPTADO</td>
     </tr>
   </tbody>
 </Table>
             </div>
             <div>
-            <Button className='botoncito2'>Crear expediente</Button>{' '}
-            <Button className='botoncito2'>Descargar todos los documentos</Button>{' '}
+            <Button className='botoncito2'>Crear nuevo punto</Button>{' '}
             </div>
         </>
         
