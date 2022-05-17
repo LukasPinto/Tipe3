22cfb783f26ed606aea38a23522ebb2c2adbe1d6
Raigou <angel.araya@alumnos.uv.cl>
2022-05-16 23:24:53 -0400

    punto solicitud del fabian
    viene con la tabla dentro de la tabla

--- Codigo_sql/Cliente/src/components/puntosSolicitud.jsx
+++ Codigo_sql/Cliente/src/components/puntosSolicitud.jsx
@@ -1,45 +1,38 @@
-import React, { useContext } from 'react';
-import { Form, Button, Table } from 'react-bootstrap';
+import React, { useContext, Component } from 'react';
+import { Form, Button, Table, Accordion, Card } from 'react-bootstrap';
 import "./css/historialSolicitud.css"
+import "./css/puntossolicitud.css"
 import { Redirect, useLocation } from 'react-router-dom';
 import listadoPuntos from '../services/puntosDireccion.service';
 import { useState, useEffect } from 'react';
-import estadoSolicitud from '../services/estadoSolicitud.service';
+
 import { useLocalStorage } from './custom/useLocalStorage';
-import listaPuntos from '../services/listaPuntos.service';
-import solicitudes from '../services/solicitudes.sevice';
+
+
+
 const PuntosSolicitud = (props) => {
   const [local, setLocal] = useLocalStorage('direccion', '')
-  const [estado, setEstado] = useLocalStorage('estado', '')
-  const [solicitud, setSolicitud] = useLocalStorage('solicitud', '')
   const [puntos, setPuntos] = useState([])
   const [actualizar, setActualizar] = useState(true)
   const [traerDatos, setTraerDatos] = useState(true)
 
   useEffect(() => {
-    
-     listaPuntos(solicitud)
-      .then((Response) => {
-        console.log(Response)
-        setPuntos(Response.data)
 
+    listadoPuntos(local)
+      .then((Response) => {
+        setPuntos(Response.data)
         setActualizar(!actualizar)
       }).
       catch(() => {
-        setPuntos([])
-        
         alert("error")
       })
   }, [traerDatos])
 
-
   return (
     <>
-      {console.log(solicitud)}
-
+      {console.log(local)}
       {(local === '') ? (<Redirect to='/vistageneral'></Redirect>) :
         <>
-
           <div>
             <p class="cajita1">Puntos de la solicitud</p>
           </div>
@@ -53,29 +46,13 @@ const PuntosSolicitud = (props) => {
 
                   <th>Puntos</th>
                   <th>Descripcion del punto</th>
-                  {(estado === "aceptado" || estado === "rechazado" || estado === "false") ? <>
-                    <th>Eliminar punto</th>
-                    <th>Editar punto</th>
-                    <th>Asignar encargado</th>
-                    <th>Editar punto</th>
-                    <th>Historial</th>
-                    <th>Fecha inicio</th>
-                    <th>Fecha termino</th>
-                    <th>Estado</th>
-                  </> :
-                    <>
-                      <th>Eliminar punto</th>
-                      <th>Editar punto</th>
-                      <th>Asignar encargado</th>
-                      <th>Editar punto</th>
-                      <th>Historial</th>
-                      <th>Fecha inicio</th>
-                      <th>Fecha termino</th>
-                      <th>Estado</th>
-                    </>
-
-                  }
-
+                  <th>Asignar encargado</th>
+                  <th>Eliminar punto</th>
+                  <th>Editar punto</th>
+                  <th>Historial</th>
+                  <th>Fecha inicio</th>
+                  <th>Fecha termino</th>
+                  <th>Estado</th>
 
                 </tr>
               </thead>
@@ -85,37 +62,55 @@ const PuntosSolicitud = (props) => {
                     <tr>
                       <td>{key + 1}</td>
                       <td>{value.titulo}</td>
-                      {(estado == "aceptado" || estado == "rechazado" || estado == "false") ?
-                        <>
-                          <td>{' solicitud cerrada'}</td>
-                          <td>{' solicitud cerrada'}</td>
-                          <td>{' solicitud cerrada'}</td>
-                          <td>{' solicitud cerrada'}</td>
-                          <td><Button variant="primary">VER</Button>{' '}</td>
-                          <td>{value.inicio.substring(0, 10)}</td>
-                          <td>{value.termino.substring(0, 10)}</td>
-                          <td>{value.estado}</td>
-                        </>
-                        :
-                        <>
-                          <td><Button variant="primary">ELIMINAR</Button>{' '}</td>
-                          <td><Button variant="primary">EDITAR</Button>{' '}</td>
-                          <td><Button variant="primary">ASIGNAR</Button>{' '}</td>
-                          <td><Button variant="primary">EDITAR</Button>{' '}</td>
-                          <td><Button variant="primary">VER</Button>{' '}</td>
-                          <td>{value.inicio.substring(0, 10)}</td>
-                          <td>{value.termino.substring(0, 10)}</td>
-                          <td>{value.estado}</td>
-
-                        </>}
+                      <td><Button variant="primary">ASIGNAR</Button>{' '}</td>
+                      <td><Button variant="primary">ELIMINAR</Button>{' '}</td>
+                      <td><Button variant="primary">EDITAR</Button>{' '}</td>
 
 
+                      <td><Accordion defaultActiveKey="0" variant="primary">
+                        <Accordion.Item eventKey="0" variant="primary">
+                          <Accordion.Header variant="primary">Ver</Accordion.Header>
+                          <Accordion.Body variant="primary">
+                            
+                              <Table className= "fondoTabla" striped bordered hover size="s">
+                                <thead>
+                                  <tr>
+
+                                    <th>Numero Intento</th>
+                                    <th>Encargado</th>
+                                    <th>Estado</th>
+                                    <th>Detalle</th>
+
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <td>Intento x</td>
+                                  <td>Rechazado</td>
+                                  <td>n/a</td>
+
+
+                                  <td><Button className="boton-centro button" type="button">Ver</Button>{' '}</td>
+
+                                </tbody>
+                              </Table>
+                            
+                          </Accordion.Body>
+                        </Accordion.Item>
+
+                      </Accordion> </td>
+
+
+                      <td>{value.inicio.substring(0, 10)}</td>
+                      <td>{value.termino.substring(0, 10)}</td>
+                      <td>{value.estado}</td>
                     </tr>
                   </>
                 })}
               </tbody>
             </Table>
           </div>
+
+
           <div>
             <Button className='botoncito2'>Crear nuevo punto</Button>{' '}
           </div>
@@ -130,11 +125,5 @@ const PuntosSolicitud = (props) => {
 
   );
 };
-
-
-
-
-
-
 
 export default PuntosSolicitud;
