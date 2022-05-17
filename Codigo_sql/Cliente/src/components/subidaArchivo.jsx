5138bb65585ec3c7206ffe8195c4d7cf2f956e4a
LukasPinto <lukas.pinto@alumnos.uv.cl>
2022-05-17 04:50:39 -0400

    subida de archivos cliente
    subida de archivos en la parte del cliente lista, falta mandar los datos al back

--- Codigo_sql/Cliente/src/components/subidaArchivo.jsx
+++ Codigo_sql/Cliente/src/components/subidaArchivo.jsx
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState,useRef} from 'react';
 import { Form, Button, Table, Badge } from 'react-bootstrap';
 import "./css/subidaArchivo.css"
 
 
 const ResumenJuicios = (props) => {
     const [archivos, setArchivos]=useState([]);
-    
+    const [drag,setDrag] = useState(null)
+    const fileRef = useRef();
     const subirArchivos=async e=>{
-       
+
         await setArchivos([...archivos,...e.target.files])
         //console.log(e.target.files)
     }
+    const dragStarted= e=>{
+        if(drag){
+            setDrag(false)
+        }
+        else{
+            setDrag(true)
+        }
+        
    
+        
+    }
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setDrag(null)
+        }, 500)
+    
+        return () => clearTimeout(timer)
+      }, [drag])
+ 
     return (
     <>
     {console.log(archivos)}
-        <div>
+        <div className="container"  onDragOverCapture={dragStarted} >
+        <div >
             <p class="cajita1">Subida de Archivos</p>
         </div>
         <div className='caja2'>
@@ -28,19 +49,33 @@ const ResumenJuicios = (props) => {
                 <Form.Control className="outer-control" type="text" name='titulo' />
                 <Form.Label>Tipo de archivo</Form.Label>
 
+                {(drag == null)? <>
                 
+                    <div className='container'>
+                    <Form.Control className="drop-area "   placeholder='Suba un archivo o arrastre aqui' disabled onChange={subirArchivos}  ></Form.Control>
+                    <Form.Control className="drop-area " multiple type='file'  name="archivo" placeholder='Suba un archivo o arrastre aqui'  onChange={subirArchivos} ref={fileRef} hidden></Form.Control>
+                    <button className="botoncito1 button" variant="outline-success" onClick={(e) => {
+                        e.preventDefault()
+                        fileRef.current.click()}}>
+        Custom File Input Button
+      </button>
+                    </div>
+                </>:<>
+                <div className='container'>
+                    <Form.Control className="drop-area" type="file"   multiple  name="archivo" onChange={subirArchivos}  />
+                    </div>
+                
+                </>}
                     
-                    <Form.Control className="drop-area" type="file"   multiple placeholder="Arrastre o suba un archivo" name="archivo"  onChange={subirArchivos}> 
-                    </Form.Control>
  
-                <button variant="outline-success" type="submit" className="botoncito1 button" multiple>Crear punto</button>
+                <button variant="outline-success" type="submit" className="botoncito1 button" >Crear punto</button>
             
             </Form>
             
         </div>
+      
+        </div>
 
-                
-               
               
             
             
