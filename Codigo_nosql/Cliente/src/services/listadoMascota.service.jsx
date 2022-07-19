4a4b660775a64e56d82e95ff381100ce9cabcfb6
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-19 11:23:37 -0400

    Delete Codigo_nosql directory

--- Codigo_nosql/Cliente/src/services/listadoMascota.service.jsx
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios'
-export default function mascotasService(id){
-    return  (Axios.get(`http://localhost:3001/mascota/${id}`, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-        }
-    }))
- }
--- Codigo_nosql/Cliente/src/services/login.service.jsx
+++
@@ -1,4 +0,0 @@
-import Axios from "axios"
-export default function loginService({correo,clave}){
-   return Axios.post(`http://localhost:3001/inicioSesion`,{ correo , clave})
-}
