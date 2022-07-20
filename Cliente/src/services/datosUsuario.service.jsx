e0122cc557ef0fda3999bac39a90a7273a21efa6
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 17:16:45 -0400

    Update datosUsuario.service.jsx

--- Cliente/src/services/datosUsuario.service.jsx
+++ Cliente/src/services/datosUsuario.service.jsx
@@ -3,7 +3,7 @@ import Axios from 'axios'
 
 export default function DatosUsuario(id_cargo,id_empl_direccion){
 
-    return (Axios.post(`http://localhost:3110/direccion/usuario`, {
+    return (Axios.post(`http://10.100.6.6:3110/direccion/usuario`, {
             id_cargo:id_cargo,
             id_empl_direccion:id_empl_direccion
         }, {
