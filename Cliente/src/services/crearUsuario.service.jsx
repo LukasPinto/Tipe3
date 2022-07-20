c1ebc183948a0261ef613d0998c9a46485b83a95
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 17:16:11 -0400

    Update crearUsuario.service.jsx

--- Cliente/src/services/crearUsuario.service.jsx
+++ Cliente/src/services/crearUsuario.service.jsx
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 export default function crearUsuario(id_direccion,id_cargo,nombre,rut,correo,clave,id_cargo_admin){
     
-    return (Axios.post(`http://localhost:3110/crearUsuario`, {
+    return (Axios.post(`http://10.100.6.6:3110/crearUsuario`, {
     admin:{
         id_cargo:id_cargo_admin
     },
