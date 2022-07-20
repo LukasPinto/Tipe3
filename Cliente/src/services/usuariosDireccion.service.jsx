8cf3c0104f3d1cf86cb20e11ddd5ca2f56567e50
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 17:19:53 -0400

    Update usuariosDireccion.service.jsx

--- Cliente/src/services/usuariosDireccion.service.jsx
+++ Cliente/src/services/usuariosDireccion.service.jsx
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 export default function usuariosPorDireccion(id_direccion){
     
-    return (Axios.post(`http://localhost:3110/direccion/usuarios`, {
+    return (Axios.post(`http://10.100.6.6:3110/direccion/usuarios`, {
     id_direccion:id_direccion
  
 }
