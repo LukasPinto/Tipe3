de4556f4f2b16976bb82888da2becce168d6f431
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 16:27:29 -0400

    Update listadoPlantilla.service.jsx

--- Cliente/src/services/listadoPlantilla.service.jsx
+++ Cliente/src/services/listadoPlantilla.service.jsx
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 export default function ListadoPlantilla(id_punto){
     
-    return (Axios.get(`http://localhost:3001/plantilla/${id_punto}`, 
+    return (Axios.get(`http://localhost:3110/plantilla/${id_punto}`, 
      {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
