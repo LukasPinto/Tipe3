b86279b1b70394d3c56cb3a07888b4360978c019
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 16:26:05 -0400

    Update descargarPlantilla.service.jsx

--- Cliente/src/services/descargarPlantilla.service.jsx
+++ Cliente/src/services/descargarPlantilla.service.jsx
@@ -3,7 +3,7 @@ import Axios from 'axios'
 
 export default function DescargarPlantilla(id_archivo_plantilla){
 
-    return (Axios.get(`http://localhost:3001/descargar/plantilla`, {
+    return (Axios.get(`http://localhost:3110/descargar/plantilla`, {
         responseType: 'blob',
         id_archivo_plantilla:id_archivo_plantilla
         }, {
