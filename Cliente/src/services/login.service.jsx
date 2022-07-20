28ca141d2e8a1d60d3966c01761a59415b417b10
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 17:10:37 -0400

    Update login.service.jsx

--- Cliente/src/services/login.service.jsx
+++ Cliente/src/services/login.service.jsx
@@ -1,4 +1,4 @@
 import Axios from "axios"
 export default function loginService({correo,clave}){
-   return Axios.post(`http://localhost:3110/login`,{ correo , clave})
+   return Axios.post(`http://10.100.6.6:3110/login`,{ correo , clave})
 }
