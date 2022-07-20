35332a2585990b6d8780d86e89e803233dcbc585
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 17:10:09 -0400

    Update PuntosUsuario.service.jsx

--- Cliente/src/services/PuntosUsuario.service.jsx
+++ Cliente/src/services/PuntosUsuario.service.jsx
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 export default function PuntosUsuario(id_empl_municipal){
     
-    return (Axios.get(`http://localhost:3110/solicitud/usuario/${id_empl_municipal}`, 
+    return (Axios.get(`http://10.100.6.6:3110/solicitud/usuario/${id_empl_municipal}`, 
      {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
