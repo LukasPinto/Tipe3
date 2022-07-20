10a7315160f49ca3f959a124559c1bb50be1ca2f
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 16:26:35 -0400

    Update estadoSolicitud.service.jsx

--- Cliente/src/services/estadoSolicitud.service.jsx
+++ Cliente/src/services/estadoSolicitud.service.jsx
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 export default function estadoSolicitud(id_solicitud){
     
-    return (Axios.post(`http://localhost:3001/estado/solicitud`, {
+    return (Axios.post(`http://localhost:3110/estado/solicitud`, {
     id_solicitud:id_solicitud
  
 }
