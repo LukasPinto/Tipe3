acb049262e52c56bf3fc625970c24dec04b221fc
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 17:17:12 -0400

    Update eliminarUsuario.service.jsx

--- Cliente/src/services/eliminarUsuario.service.jsx
+++ Cliente/src/services/eliminarUsuario.service.jsx
@@ -3,7 +3,7 @@ import Axios from 'axios'
 
 export default function EliminiarUsuarioDireccion(id_cargo,id_empl_direccion){
 
-    return (Axios.post(`http://localhost:3110/direccion/usuario/borrar`, {
+    return (Axios.post(`http://10.100.6.6:3110/direccion/usuario/borrar`, {
             id_cargo:id_cargo,
             id_empl_direccion:id_empl_direccion
         }, {
