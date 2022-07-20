d5d98ef82f8b59c517f1c99955a690d3671eb543
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 17:16:32 -0400

    Update datosPunto.service.jsx

--- Cliente/src/services/datosPunto.service.jsx
+++ Cliente/src/services/datosPunto.service.jsx
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 export default function DatosPunto(id_punto){
     
-    return (Axios.get(`http://localhost:3110/punto/${id_punto}`, 
+    return (Axios.get(`http://10.100.6.6:3110/punto/${id_punto}`, 
      {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
