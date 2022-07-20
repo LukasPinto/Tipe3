f275163d41ceb7e3289d21916355f6b2f0245b63
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 16:26:52 -0400

    Update intentos.service.jsx

--- Cliente/src/services/intentos.service.jsx
+++ Cliente/src/services/intentos.service.jsx
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 export default function Intentos(id_punto){
     
-    return (Axios.get(`http://localhost:3001/intentos/usuario/${id_punto}`, 
+    return (Axios.get(`http://localhost:3110/intentos/usuario/${id_punto}`, 
      {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
