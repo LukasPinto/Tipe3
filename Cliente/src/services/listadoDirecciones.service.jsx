fa0d3c8255bcd95c18ca768bee3325f9aa4af0a0
LukasPinto <73002372+LukasPinto@users.noreply.github.com>
2022-07-20 16:27:17 -0400

    Update listadoDirecciones.service.jsx

--- Cliente/src/services/listadoDirecciones.service.jsx
+++ Cliente/src/services/listadoDirecciones.service.jsx
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 export default function direccionesService(){
-    return  (Axios.get(`http://localhost:3001/direcciones`, {
+    return  (Axios.get(`http://localhost:3110/direcciones`, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
         }
